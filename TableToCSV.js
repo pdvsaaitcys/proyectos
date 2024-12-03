c71ad9358dbc460e83c24988c25156a7be9837a4
pdvsaaitcys <pdvsaaitcys@gmail.com>
2024-12-02 20:34:34 -0400

    Create TableToCSV.js

---
+++ TableToCSV.js
@@ -0,0 +1,32 @@
+class TableToCSV {
+    constructor(tableId, fileName) {
+        this.tableId = tableId;
+        this.fileName = fileName;
+    }
+
+    export() {
+        const table = document.getElementById(this.tableId);
+        let csv = [];
+        for (let row of table.rows) {
+            let rowData = [];
+            for (let cell of row.cells) {
+                rowData.push(cell.innerText);
+            }
+            csv.push(rowData.join(','));
+        }
+        const csvString = csv.join('\n');
+        this.downloadCSV(csvString);
+    }
+
+    downloadCSV(csvString) {
+        const blob = new Blob([csvString], { type: 'text/csv;charset=utf-8;' });
+        const url = URL.createObjectURL(blob);
+        const link = document.createElement('a');
+        link.setAttribute('href', url);
+        link.setAttribute('download', this.fileName);
+        link.style.visibility = 'hidden';
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+    }
+}
